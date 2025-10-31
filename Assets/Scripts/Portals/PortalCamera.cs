37dbc00b724cda66acb882465f9870a932ca220e
Adri0211 <Adrios.0211@gmail.com>
2025-10-31 17:42:56 +0100

    Correct Portal Rendering

--- Assets/Scripts/Portals/PortalCamera.cs
+++ Assets/Scripts/Portals/PortalCamera.cs
@@ -26,14 +26,14 @@ namespace Portals
             var unProjectedCameraPosition = otherPortal.transform.TransformPoint(-relativePlayerPosition);
             transform.position = unProjectedCameraPosition;
             
-            var relativePlayerRotation = Quaternion.Inverse(portal.transform.rotation) * _cameraTransform.rotation;
-            var unProjectedCameraRotation = otherPortal.transform.rotation * relativePlayerRotation;
-            transform.rotation = unProjectedCameraRotation * Quaternion.Euler(0, 180, 0);
+            // var relativePlayerRotation = Quaternion.Inverse(portal.transform.rotation) * _cameraTransform.rotation * Quaternion.Euler(0, 180, 0);
+            // var unProjectedCameraRotation = otherPortal.transform.rotation * relativePlayerRotation;
+            // transform.rotation = unProjectedCameraRotation;
             
-            // var relativePlayerRotation = portal.transform.InverseTransformDirection(_cameraTransform.rotation.eulerAngles);
-            // var unProjectedCameraRotation = otherPortal.transform.TransformDirection(relativePlayerRotation);
-            // transform.rotation = Quaternion.Euler(unProjectedCameraRotation);
-            
+            var relativePlayerRotation = portal.transform.InverseTransformDirection(_cameraTransform.forward);
+            relativePlayerRotation = Quaternion.Euler(0, 180, 0) * relativePlayerRotation;
+            var unProjectedCameraRotation = otherPortal.transform.TransformDirection(relativePlayerRotation);
+            transform.forward = unProjectedCameraRotation;
             
         }
     }
