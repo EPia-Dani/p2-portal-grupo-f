6cc0a92546044ff54e6a2b350c036aa82f99d9a3
cakeruu <150198610+cakeruu@users.noreply.github.com>
2025-11-12 13:03:32 +0100

    Polish spawner

--- Assets/Scripts/GravityGun/CubeSpawner.cs
+++ Assets/Scripts/GravityGun/CubeSpawner.cs
@@ -1,35 +1,39 @@
 using System.Collections.Generic;
 using UnityEngine;
-using Core.EventBus;
 using Player;
+using PrimeTween;
 
 namespace GravityGun
 {
-
     public class CubeSpawner : MonoBehaviour, Interactable
     {
-        public GameObject cubePrefab;
-        public float spawnClearance = 0.01f;
-
-        public float pickRange = 5f;
-
-        [Header("Limit")]
         public int maxCubes;
+        public GameObject cubePrefab;
+        public Vector3 spawnRelativePosition;
+        private readonly Queue<GameObject> spawnedCubes = new();
 
-        private readonly List<GameObject> spawnedCubes = new List<GameObject>();
-        
 
         private void SpawnCube()
         {
-            spawnedCubes.RemoveAll(item => item == null);
-            Collider spawnerCol = GetComponent<Collider>();
-            var topY = spawnerCol.bounds.max.y;
-            GameObject cube = Instantiate(cubePrefab, transform.position, Quaternion.identity);
-            Collider cubeCol = cube.GetComponent<Collider>();
-            float halfHeight = cubeCol.bounds.extents.y;
-            Vector3 spawnPos = new Vector3(transform.position.x, topY + halfHeight + spawnClearance, transform.position.z);
-            cube.transform.position = spawnPos;
-            spawnedCubes.Add(cube);
+            if (spawnedCubes.Count >= maxCubes)
+            {
+                var firstCube = spawnedCubes.Dequeue();
+                Tween.Scale(firstCube.transform, Vector3.zero, 0.2f, Ease.InBack).OnComplete(() =>
+                {
+                    Destroy(firstCube);
+                });
+            }
+            var cube = Instantiate(cubePrefab, transform.position + spawnRelativePosition, Quaternion.identity);
+            var originalScale = cube.transform.localScale;
+            cube.transform.localScale = Vector3.zero;
+            Tween.Scale(cube.transform, originalScale, 0.5f, Ease.OutBack);
+            spawnedCubes.Enqueue(cube);
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(transform.position + spawnRelativePosition, 0.1f);
         }
 
         public void Interact()
