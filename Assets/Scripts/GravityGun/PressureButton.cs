f5a7fb1a9e988ba29259c17e9d95aac03f98c76f
cakeruu <150198610+cakeruu@users.noreply.github.com>
2025-11-13 09:26:58 +0100

    button working

--- Assets/Scripts/GravityGun/PressureButton.cs
+++ Assets/Scripts/GravityGun/PressureButton.cs
@@ -6,35 +6,18 @@ namespace GravityGun
     {
         
         private Vector3 initialPosition;
-        private bool pressed;
+        private bool _pressed;
         private ConfigurableJoint joint;
         void Start()
         {
-            Debug.Log("Button");
-            joint = GetComponent<ConfigurableJoint>();
-            initialPosition = transform.localPosition;
-            pressed = false;
+            _pressed = false;
         }
 
-        void Update()
+        public void SetPressed(bool p)
         {
-            float limit = Mathf.Abs(joint.linearLimit.limit);
-            Vector3 delta = transform.localPosition - initialPosition;
-            float proj = Vector3.Dot(delta, Vector3.up.normalized);
-            
-            Debug.Log(pressed);
-            Debug.Log(proj);
-            
-            if (proj > limit)
-            {
-                pressed = true;
-                
-            }
-            else
-            {
-                pressed = false;
-                
-            }
+            string log = p ? "pressed" : "unpressed";
+            Debug.Log($"Button {log}");
+            _pressed = p;
         }
     }
 }
